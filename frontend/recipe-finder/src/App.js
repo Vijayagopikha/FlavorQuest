703c22a93be799669bdc3e4291d882f128d9b178
VAISHNAVI K <vaishnavikarthikeyan108@gmail.com>
2024-10-18 09:17:49 +0530

    push

--- frontend/recipe-finder/src/App.js
+++ frontend/recipe-finder/src/App.js
@@ -1,19 +1,19 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Signup from './components/Signup';
-import Login from './components/Login';
-
+import Recipes from './pages/Recipes';
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 const App = () => {
-  return (
-    <Router>
-      <div className="App">
+    return (
+      <Router>
         <Routes>
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/recipe" element={<Recipes />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
-      </div>
-    </Router>
+      </Router>
   );
 };
 
